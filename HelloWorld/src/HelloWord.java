bd25b4501bb7e7999c211e4b686763eebc185713
User <User@YD02-10>
2020-01-21 12:37:11 +0900

    0121

---
+++ HelloWorld/src/HelloWord.java
@@ -0,0 +1,12 @@
+
+public class HelloWord {
+	public static void main(String[] args) {
+		int age = 28;
+		int year = 2020; //int 정수는 담수 함수
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("올해는" + year+"년");
+		System.out.println("내 나이는"+ age+"입니다");
+		System.out.println(year+age);
+	}
+}
+//sout 컨트롤 스페이스바 누르면 system.out.println 자동완성
