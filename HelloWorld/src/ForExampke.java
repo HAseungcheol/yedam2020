2610fae87290169b49cbe88d3504182efc2b9b7b
User <User@YD02-10>
2020-01-22 17:52:44 +0900

    20200122

---
+++ HelloWorld/src/ForExampke.java
@@ -0,0 +1,42 @@
+
+public class ForExampke {
+public static void main(String[] args) {
+
+		for(int i=0; i<26; i++) {
+		char chr1=65;
+		chr1=(char)(chr1+i); //이구문 안쓰면 밑에 구문만 쓰면 된다
+		System.out.println(chr1);
+			//System.out.println(chr1++)
+		}
+		boolean trueOrFalse = false;
+		if (!trueOrFalse) {
+			System.out.println("참입니다");
+		
+		boolean trueorfalse = false;
+		if (3 !=5 ) {
+		System.out.println("참입니다");
+		}
+	}			
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
